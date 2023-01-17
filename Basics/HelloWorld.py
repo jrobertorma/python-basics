1cd16712875e30ce4304e8e3675f0140daee3827
jrobertorma <jrobertorma@gmail.com>
2023-01-17 10:04:39 -0600

    Add hello world script

---
+++ Basics/HelloWorld.py
@@ -0,0 +1,9 @@
+# printing stuff
+print("Hello world (from Python)")
+
+# printing stuff with a loop, notice the ':' at the top of the for loop
+friends = ['Miguel Angel', 'Rafael', 'Donatelo', 'Leonardo']
+for friend in friends:
+    print("Yo, " + friend)
+
+# we also want to have an empty last line
