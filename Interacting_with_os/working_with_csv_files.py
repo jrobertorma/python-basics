36bbc8d73fa41755fa1b50f833a3f6c38efa831f
jrobertorma <jrobertorma@gmail.com>
2023-02-20 20:30:42 -0600

    Add module review script

--- Interacting_with_os/working_with_csv_files.py
+++ Interacting_with_os/working_with_csv_files.py
@@ -93,3 +93,43 @@ def contents_of_file(filename):
 #Call the function
 print(contents_of_file("flowers.csv"))
 
+
+# Final project, a script that reads a file, parses it and builds a report using the provided file's data
+
+#!/usr/bin/env python3
+import csv
+
+
+def read_employees(csv_file_location):
+  employee_list = []
+  with open(csv_file_location) as csv_file:
+    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+    reader = csv.DictReader(csv_file,dialect = 'empDialect')
+    for data in reader:
+      employee_list.append(data)
+  return employee_list
+
+
+def process_data(employee_list):
+  department_list = []
+  for employee_data in employee_list:
+    department_list.append(employee_data['Department'])
+
+  department_data = {}
+  for department_name in set(department_list):
+    department_data[department_name] = department_list.count(department_name)
+
+  return department_data
+
+
+def write_report(dictionary, report_file):
+  with open(report_file, "w+") as f:
+    for k in sorted(dictionary):
+      f.write(str(k)+':'+str(dictionary[k])+'\n')
+    f.close()
+
+
+employee_list = read_employees("/home/student-03-e0ad12c300c2/data/employees.csv")
+dictionary = process_data(employee_list)
+write_report(dictionary, '/home/student-03-e0ad12c300c2/test_report.txt')
+
